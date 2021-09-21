05e99993d14fe571f0a8dca37979652fd3243cd5
SmilyChip <angelo.verae@gmail.com>
2021-09-21 00:05:25 -0300

    det_pato terminado

--- cap3/det_pato.py
+++ cap3/det_pato.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     # Se leen los argumentos de entrada
     parser = argparse.ArgumentParser()
     parser.add_argument('--env-name', default="Duckietown-udem1-v1")
-    parser.add_argument('--map-name', default='udem1')
+    parser.add_argument('--map-name', default='angelos_map')
     parser.add_argument('--distortion', default=False, action='store_true')
     parser.add_argument('--draw-curve', action='store_true', help='draw the lane following curve')
     parser.add_argument('--draw-bbox', action='store_true', help='draw collision detection bounding boxes')
@@ -60,8 +60,8 @@ if __name__ == '__main__':
 
     # Parametros para el detector de patos
     # Se debe encontrar el rango apropiado
-    lower_yellow = np.array([H_m, S_m, V_m])
-    upper_yellow = np.array([H_M, S_M, V_M])
+    lower_yellow = np.array([10,240,130])
+    upper_yellow = np.array([60,255,255])
     min_area = 2500
 
     while True:
@@ -87,13 +87,13 @@ if __name__ == '__main__':
         ### CÓDIGO DE DETECCIÓN POR COLOR ###
 
         #Transformar imagen a espacio HSV
-
+        img_out = cv2.cvtColor(obs, cv2.COLOR_RGB2HSV)
 
         # Filtrar colores de la imagen en el rango utilizando
-
+        mask = cv2.inRange(img_out, lower_yellow, upper_yellow)     
 
         # Bitwise-AND entre máscara (mask) y original (obs) para visualizar lo filtrado
-
+        filtered = cv2.bitwise_and(obs , obs , mask = mask)
 
         # Se define kernel para operaciones morfológicas
         kernel = np.ones((5,5),np.uint8)
@@ -102,28 +102,29 @@ if __name__ == '__main__':
         # Esto corresponde a hacer un Opening
         # https://docs.opencv.org/trunk/d9/d61/tutorial_py_morphological_ops.html
         #Operacion morfologica erode
+        mask_out = cv2.erode(mask , kernel, iterations = 1)
 
         #Operacion morfologica dilate
-
+        mask_out = cv2.dilate(mask , kernel, iterations = 1)
 
         # Busca contornos de blobs
         # https://docs.opencv.org/trunk/d3/d05/tutorial_py_table_of_contents_contours.html
-
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         # Iterar sobre contornos y dibujar bounding box de los patos
         for cnt in contours:
             # Obtener rectangulo que bordea un contorno
-
+            x, y, w, h = cv2.boundingRect(cnt)
             #Filtrar por area minima
-            if AREA > min_area: # DEFINIR AREA
+            if x*y > min_area: # DEFINIR AREA
                 #Dibujar rectangulo en el frame original
-
+                cv2.rectangle(obs , (x, y), (x+w, y+h), (255,0,128) , 2 ) 
 
         # Se muestra en una ventana llamada "patos" la observación del simulador
         # con los bounding boxes dibujados
         cv2.imshow('patos', cv2.cvtColor(obs, cv2.COLOR_RGB2BGR))
         # Se muestra en una ventana llamada "filtrado" la imagen filtrada
-        cv2.imshow('filtrado', image)
+        cv2.imshow('filtrado', cv2.cvtColor(filtered , cv2.COLOR_RGB2BGR) )
 
 
     # Se cierra el environment y termina el programa
